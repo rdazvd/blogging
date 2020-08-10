5bdc041abd000658fdd28dc8af11308cab2ac08c
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-08-10 18:46:30 -0300

    Feat: pass BlogPost Context to ShowScreen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
 
-const ShowScreen = () => (
-  <View>
-    <Text>Show Screen</Text>
-  </View>
-);
+const ShowScreen = ({ navigation }) => {
+  const { state } = React.useContext(Context);
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam('id')
+  );
+
+  return (
+    <View>
+      <Text>{blogPost.title}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({});
 
