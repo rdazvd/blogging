1d8c67a884fad3d9a7a7e3fa6632ec54b805e03b
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-08-10 21:48:55 -0300

    Feat: created CreateScreen

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
 import ShowScreen from './src/screens/ShowScreen';
-import {} from './src/context/BlogContext';
 import { Provider } from './src/context/BlogContext';
+import CreateScreen from './src/screens/CreateScreen';
 
 const navigator = createStackNavigator(
   {
     Index: IndexScreen,
     Show: ShowScreen,
+    Create: CreateScreen,
   },
   {
     initialRouteName: 'Index',
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -38,6 +38,14 @@ const IndexScreen = ({ navigation }) => {
   );
 };
 
+IndexScreen.navigationOptions = ({ navigation }) => ({
+  headerRight: () => (
+    <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+      <Feather name="plus" size={30} size={30} />
+    </TouchableOpacity>
+  ),
+});
+
 const styles = StyleSheet.create({
   row: {
     flexDirection: 'row',
