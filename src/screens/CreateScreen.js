91887b9392a65c95328dbcce66af09d9ceb63961
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-08-10 22:08:19 -0300

    Feat: added local state and styles to CreateScreen

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
 
 const CreateScreen = () => {
+  const [title, setTitle] = React.useState('');
+  const [content, setContent] = React.useState('');
+
   return (
     <View>
-      <Text>Create Screen</Text>
+      <Text style={styles.label}>Enter Title</Text>
+      <TextInput
+        value={title}
+        onChangeText={(userInput) => setTitle(userInput)}
+        style={styles.input}
+      />
+      <Text style={styles.label}>Enter Content</Text>
+      <TextInput
+        value={content}
+        onChangeText={(userInput) => setContent(userInput)}
+        style={styles.input}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 15,
+    padding: 5,
+    margin: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5,
+  },
+});
 
 export default CreateScreen;
